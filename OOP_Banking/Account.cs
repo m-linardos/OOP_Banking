aa5f5b6bc8b86ea336a06e6ebaacc274e3850711
Marianne Linardos <mlinardos.edu@gmail.com>
2019-07-29 13:38:32 -0400

    created accoun class

---
+++ OOP_Banking/Account.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Banking {
+
+    public class Account {
+
+        public string Number { get; set; }
+        /*public*/
+        private decimal Balance { get; set; } = 0.0M;   //  changed public to private / numbers with decimals are doubles and floats / add 'M' to it to treat as a decimal, not a float / Can also be '0'
+        public string Name { get; set; }
+        public bool IsActive { get; set; } = true;      // default true
+
+
+        public void Deposit(decimal Amount) {                    // parameter for amount
+            this.Balance += Amount;
+            if (Amount <= 0) {
+                Console.WriteLine("Deposit amount must be greater than 0");
+            } else {
+                this.Balance += Amount;
+            }
+
+        }
+    }
+        //public void Withdraw(decimal Amount) {
+        //    if(this.Balance >= Amount) {            
+        //    this.Balance -= Amount;
+        //    }
+            if (Amount <= 0) {
+                Console.WriteLine("Deposit amount must be greater than 0");
+            }
+            else {
+                if(this.Balance >= Amount) {
+        this.Balance -= Amount;                
+                }
+            }
+        }
+
+        public decimal GetBalance() {
+            return this.Balance;
+        }
+    }
+}
--- OOP_Banking/Program.cs
+++ OOP_Banking/Program.cs
@@ -1,9 +1,31 @@
 ﻿using System;
 
 namespace OOP_Banking {
+
     class Program {
+
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+            Account acct = new Account();           // calling default constructor from Account class / new instance
+            acct.Number = "1001";                          // (variable.propertyname) represent the instance of the Account class
+            acct.Name = "Test Acount";
+            /*acct.Balance = 0; */                // can't insert the number into balabce / initially set to 0
+            acct.Deposit(20);                      //
+            acct.Withdraw(10);
+            decimal balance = acct.GetBalance();
+            Console.WriteLine($"Account balance is {balance} (should be 10)");
+
+            acct.Deposit(-10);              // test for negative path - You can't deposit a neg. nbr
+            balance = acct.GetBalance();
+            Console.WriteLine($"Account balance is {balance} (should be 10)");
+
+            acct.Deposit(-10);              // test for negative path / produce error message
+            balance = acct.GetBalance();
+            Console.WriteLine($"Account balance is {balance} (should be 10)");
+
+            acct.Deposit(20);
+            balance = acct.GetBalance();
+            Console.WriteLine($"Account balance is {balance} (should be 10)");
         }
     }
 }
